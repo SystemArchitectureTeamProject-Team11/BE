36a3d00041b98c18e9cb2538e5868d94d878b91b
DESKTOP-7MNK8JV\HP <gytjd9516@naver.com>
2023-11-14 14:28:57 +0900

    cors 오리진 추가

--- src/main/java/SA/team11/systemarchitecture/controller/EventController.java
+++ src/main/java/SA/team11/systemarchitecture/controller/EventController.java
@@ -5,15 +5,13 @@ import SA.team11.systemarchitecture.dto.EventCond;
 import SA.team11.systemarchitecture.dto.EventOutput;
 import SA.team11.systemarchitecture.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("http://210.119.237.42:5500")
 public class EventController {
     private final EventService eventService;
     
